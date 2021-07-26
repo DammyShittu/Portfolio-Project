acff070194dd7d23ee975e4d9b1c7235f4759467
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-07-26 17:42:36 +0100

    Fix Linter Errors

---
+++ main.js
@@ -0,0 +1,22 @@
+/* eslint-disable linebreak-style */
+
+const mobileMenu = document.getElementById('menu');
+const closeIcon = document.getElementById('close-menu');
+const openIcon = document.getElementById('hamburger-icon');
+
+function menuClose() {
+  mobileMenu.style.left = '-110%';
+}
+
+function menuOpen() {
+  mobileMenu.style.left = '0';
+}
+
+document.querySelectorAll('.classList').forEach((item) => {
+  item.addEventListener('click', () => {
+    mobileMenu.style.left = '-110%';
+  });
+});
+
+closeIcon.addEventListener('click', menuClose);
+openIcon.addEventListener('click', menuOpen);

7dad0be88bd4683872a86a962b6f64d88342e37f
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-07-27 11:41:13 +0100

    Add workProjects Object To Modal.js

---
+++ js/modal.js
@@ -0,0 +1,78 @@
+const workProjects = {
+  firstProject: {
+    title: 'Multi-Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent`,
+    image: {
+      imageUrl: '../img/mobile-snapshot.png',
+      imageAlt: 'Mobile Snapshot',
+    },
+    technologies: ['Html', 'Ruby on rails', 'Css', 'Github'],
+    seeLive: 'index.html',
+    soureUrl: 'https://github.com/DammyShittu/Portfolio-Project',
+  },
+  secondProject: {
+    title: 'Multi-Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent`,
+    image: {
+      imageUrl: '../img/mobile-snapshot.png',
+      imageAlt: 'Mobile Snapshot',
+    },
+    technologies: ['Html', 'Ruby on rails', 'Css', 'Github'],
+    seeLive: 'index.html',
+    soureUrl: 'https://github.com/DammyShittu/Portfolio-Project',
+  },
+  thirdProject: {
+    title: 'Multi-Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent`,
+    image: {
+      imageUrl: '../img/mobile-snapshot.png',
+      imageAlt: 'Mobile Snapshot',
+    },
+    technologies: ['Html', 'Ruby on rails', 'Css', 'Github'],
+    seeLive: 'index.html',
+    soureUrl: 'https://github.com/DammyShittu/Portfolio-Project',
+  },
+  fourthProject: {
+    title: 'Multi-Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent`,
+    image: {
+      imageUrl: '../img/mobile-snapshot.png',
+      imageAlt: 'Mobile Snapshot',
+    },
+    technologies: ['Html', 'Ruby on rails', 'Css', 'Github'],
+    seeLive: 'index.html',
+    soureUrl: 'https://github.com/DammyShittu/Portfolio-Project',
+  },
+};

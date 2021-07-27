8d8cb226c66857a6281145616072943a9f9e7613
lily-coder <lilyondie@gmail.com>
2021-07-27 16:37:02 +0300

    Add modal html text to modal.js file

--- js/modal.js
+++ js/modal.js
@@ -76,3 +76,30 @@ const workProjects = {
     soureUrl: 'https://github.com/DammyShittu/Portfolio-Project',
   },
 };
+
+function createModal() {
+  let htmlTxt = `<section id="modal">
+                  <div class="modal-menu">
+                  <img src="./img/Icon.png" alt="Close" class="modal-close">
+                  <h2>Multi-Post Stories</h2>
+                  <div class="modal-img">
+                    <img src="./img/mobile-snapshot.png" alt="">
+                  </div>
+                  <div class="modal-body">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.</p>
+                  </div>
+                  <div class="stack-used stack-used-1">
+                    <ul>
+                      <li class="stack">Html</li>
+                      <li class="stack">Ruby on Rails</li>
+                      <li class="stack">Css</li>
+                    </ul>
+                  </div>
+                  <div class="modal-btn">
+                  <button type="submit" class="modalBtn-1">See Live
+                    <img src="./img/file.png" alt="">
+                  </button>
+                  <button type="submit" class="modalBtn-2">See Source
+                  <img src="./img/git.png" alt="">
+                  </button>`;
+}

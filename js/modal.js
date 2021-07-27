0674408b4f3ca2f387d2b9e4ceace463b0e188ab
lily-coder <lilyondie@gmail.com>
2021-07-27 17:24:28 +0300

    Add the html card to modal.js file

--- js/modal.js
+++ js/modal.js
@@ -16,7 +16,7 @@ const workProjects = {
     },
     technologies: ['Html', 'Ruby on rails', 'Css', 'Github'],
     seeLive: 'index.html',
-    soureUrl: 'https://github.com/DammyShittu/Portfolio-Project',
+    sourceUrl: 'https://github.com/DammyShittu/Portfolio-Project',
   },
   secondProject: {
     title: 'Multi-Post Stories',
@@ -35,7 +35,7 @@ const workProjects = {
     },
     technologies: ['Html', 'Ruby on rails', 'Css', 'Github'],
     seeLive: 'index.html',
-    soureUrl: 'https://github.com/DammyShittu/Portfolio-Project',
+    sourceUrl: 'https://github.com/DammyShittu/Portfolio-Project',
   },
   thirdProject: {
     title: 'Multi-Post Stories',
@@ -54,7 +54,7 @@ const workProjects = {
     },
     technologies: ['Html', 'Ruby on rails', 'Css', 'Github'],
     seeLive: 'index.html',
-    soureUrl: 'https://github.com/DammyShittu/Portfolio-Project',
+    sourceUrl: 'https://github.com/DammyShittu/Portfolio-Project',
   },
   fourthProject: {
     title: 'Multi-Post Stories',
@@ -73,20 +73,39 @@ const workProjects = {
     },
     technologies: ['Html', 'Ruby on rails', 'Css', 'Github'],
     seeLive: 'index.html',
-    soureUrl: 'https://github.com/DammyShittu/Portfolio-Project',
+    sourceUrl: 'https://github.com/DammyShittu/Portfolio-Project',
   },
 };
 
-function createModal() {
+function createCard(project, ind) {
+  let htmlTxt = `<li class="work-card project-1">
+                  <div class="square ellipse"></div>
+                  <div class="card"></div>
+                  <aside class="aside-1">
+                    <h3>${project.title}</h3>
+                    <p>${project.description}</p>
+                    <div class="stack-used">
+                      <ul>`;
+                      for (let i = 0; i < project.technologies.length; i += 1) {
+                        htmlTxt += `<li class="stack>${project.technologies[i]}</li>`;
+                      }
+          htmlTxt += `</ul>
+                    </div>
+                    <button type="submit">See Project</button>
+                  </aside>
+                </li>`
+}
+
+function createModal(project, ind) {
   let htmlTxt = `<section id="modal">
                   <div class="modal-menu">
                   <img src="./img/Icon.png" alt="Close" class="modal-close">
-                  <h2>Multi-Post Stories</h2>
+                  <h2>${project.title}</h2>
                   <div class="modal-img">
-                    <img src="./img/mobile-snapshot.png" alt="">
+                    <img src="${project.image.imageUrl}" alt="${project.image.imageAlt}">
                   </div>
                   <div class="modal-body">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.</p>
+                    <p>${project.description}</p>
                   </div>
                   <div class="stack-used stack-used-1">
                     <ul>
@@ -101,5 +120,6 @@ function createModal() {
                   </button>
                   <button type="submit" class="modalBtn-2">See Source
                   <img src="./img/git.png" alt="">
-                  </button>`;
+                  </button>
+                </section>`;
 }
